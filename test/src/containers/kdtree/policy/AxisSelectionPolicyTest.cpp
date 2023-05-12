472e01d1eb80b0ef6b7cbf91e85a5c62e6334800
Palinody <lucas.klepic@protonmail.com>
2023-05-12 23:47:32 +0200

    feat: preparation KDTreeAlgorithms and axis selection & splitting rule policy unit tests

---
+++ test/src/containers/kdtree/policy/AxisSelectionPolicyTest.cpp
@@ -0,0 +1,49 @@
+
+#include "ffcl/common/Utils.hpp"
+
+#include "Range2DBaseFixture.hpp"
+
+#include "gtest/gtest.h"
+
+#include "ffcl/containers/kdtree/policy/AxisSelectionPolicy.hpp"
+
+template <typename DataType>
+class AxisSelectionPolicyTestFixture : public Range2DBaseFixture<DataType> {
+  public:
+    void SetUp() override {
+        if constexpr (std::is_integral_v<DataType> && std::is_signed_v<DataType>) {
+            lower_bound_ = -1;
+            upper_bound_ = 1;
+
+        } else if constexpr (std::is_integral_v<DataType> && std::is_unsigned_v<DataType>) {
+            lower_bound_ = 0;
+            upper_bound_ = 1;
+
+        } else if constexpr (std::is_floating_point_v<DataType>) {
+            lower_bound_ = -1;
+            upper_bound_ = 1;
+        }
+        min_n_samples_  = 1;
+        max_n_samples_  = 10;
+        n_features_     = 1;
+        n_random_tests_ = 5;
+    }
+
+  protected:
+    DataType    lower_bound_;
+    DataType    upper_bound_;
+    std::size_t min_n_samples_;
+    std::size_t max_n_samples_;
+    std::size_t n_features_;
+    std::size_t n_random_tests_;
+};
+
+using DataTypes = ::testing::Types<int, std::size_t, float, double>;
+TYPED_TEST_SUITE(AxisSelectionPolicyTestFixture, DataTypes);
+
+TYPED_TEST(AxisSelectionPolicyTestFixture, MainTest) {}
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ test/src/containers/kdtree/policy/SplittingRulePolicyTest.cpp
@@ -0,0 +1,48 @@
+#include "ffcl/common/Utils.hpp"
+
+#include "Range2DBaseFixture.hpp"
+
+#include "gtest/gtest.h"
+
+#include "ffcl/containers/kdtree/policy/SplittingRulePolicy.hpp"
+
+template <typename DataType>
+class SplittingRulePolicyTestFixture : public Range2DBaseFixture<DataType> {
+  public:
+    void SetUp() override {
+        if constexpr (std::is_integral_v<DataType> && std::is_signed_v<DataType>) {
+            lower_bound_ = -1;
+            upper_bound_ = 1;
+
+        } else if constexpr (std::is_integral_v<DataType> && std::is_unsigned_v<DataType>) {
+            lower_bound_ = 0;
+            upper_bound_ = 1;
+
+        } else if constexpr (std::is_floating_point_v<DataType>) {
+            lower_bound_ = -1;
+            upper_bound_ = 1;
+        }
+        min_n_samples_  = 1;
+        max_n_samples_  = 10;
+        n_features_     = 1;
+        n_random_tests_ = 5;
+    }
+
+  protected:
+    DataType    lower_bound_;
+    DataType    upper_bound_;
+    std::size_t min_n_samples_;
+    std::size_t max_n_samples_;
+    std::size_t n_features_;
+    std::size_t n_random_tests_;
+};
+
+using DataTypes = ::testing::Types<int, std::size_t, float, double>;
+TYPED_TEST_SUITE(SplittingRulePolicyTestFixture, DataTypes);
+
+TYPED_TEST(SplittingRulePolicyTestFixture, MainTest) {}
+
+int main(int argc, char** argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
