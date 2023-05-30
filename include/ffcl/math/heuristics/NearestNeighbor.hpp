fc16cacc428acfa9e2a8cfc4aefba7e5386d8fdd
Palinody <lucas.klepic@protonmail.com>
2023-05-30 22:48:14 +0200

    refacto/tests

--- include/ffcl/math/heuristics/NearestNeighbor.hpp
+++ include/ffcl/math/heuristics/NearestNeighbor.hpp
@@ -79,55 +79,6 @@ std::pair<ssize_t, typename SamplesIterator::value_type> nearest_neighbor_indexe
     return {current_nearest_neighbor_index, current_nearest_neighbor_distance};
 }
 
-template <typename IndicesIterator, typename SamplesIterator>
-std::pair<ssize_t, typename SamplesIterator::value_type> nearest_neighbor_indexed_range(
-    const IndicesIterator& index_first,
-    const IndicesIterator& index_last,
-    const SamplesIterator& samples_first,
-    const SamplesIterator& samples_last,
-    std::size_t            n_features,
-    std::size_t            sample_index_query,
-    IndicesIterator        exclusion_index_first,
-    IndicesIterator        exclusion_index_last) {
-    using DataType = typename SamplesIterator::value_type;
-
-    common::utils::ignore_parameters(samples_last);
-
-    const std::size_t n_samples = std::distance(index_first, index_last);
-
-    if (n_samples > 1) {
-        ssize_t current_nearest_neighbor_index    = -1;
-        auto    current_nearest_neighbor_distance = common::utils::infinity<DataType>();
-
-        for (std::size_t index = 0; index < n_samples; ++index) {
-            const std::size_t candidate_nearest_neighbor_index = index_first[index];
-
-            if (candidate_nearest_neighbor_index != sample_index_query &&
-                common::utils::is_element_not_in(
-                    exclusion_index_first, exclusion_index_last, candidate_nearest_neighbor_index)) {
-                const DataType candidate_nearest_neighbor_distance =
-                    math::heuristics::auto_distance(samples_first + sample_index_query * n_features,
-                                                    samples_first + sample_index_query * n_features + n_features,
-                                                    samples_first + candidate_nearest_neighbor_index * n_features);
-
-                if (candidate_nearest_neighbor_distance < current_nearest_neighbor_distance) {
-                    current_nearest_neighbor_index    = candidate_nearest_neighbor_index;
-                    current_nearest_neighbor_distance = candidate_nearest_neighbor_distance;
-                }
-            }
-        }
-        return {current_nearest_neighbor_index, current_nearest_neighbor_distance};
-
-    } else if (n_samples == 1 && index_first[0] != sample_index_query &&
-               common::utils::is_element_not_in(exclusion_index_first, exclusion_index_last, index_first[0])) {
-        return {index_first[0],
-                math::heuristics::auto_distance(samples_first + sample_index_query * n_features,
-                                                samples_first + sample_index_query * n_features + n_features,
-                                                samples_first + index_first[0] * n_features)};
-    }
-    return {-1, common::utils::infinity<DataType>()};
-}
-
 template <typename SamplesIterator>
 void update_nearest_neighbors_indices_buffer(const SamplesIterator&    samples_first,
                                              const SamplesIterator&    samples_last,
@@ -137,6 +88,8 @@ void update_nearest_neighbors_indices_buffer(const SamplesIterator&    samples_f
                                              std::size_t               n_neighbors,
                                              std::vector<std::size_t>& indices_buffer,
                                              std::vector<typename SamplesIterator::value_type>& distances_buffer) {
+    // DRAFT FUNCTION DO NOT USE. NEEDS A PRIORITY QUEUE
+
     // N.B.: keep the elments sorted and check if a candidate is closer than the furthest current nearest neighbors. If
     // its the case, place it at the correct position.
     // Checks whether one of the elements of the buffer is larger than the sample candidate and replace the furthest
