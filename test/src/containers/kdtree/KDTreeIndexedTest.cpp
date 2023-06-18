3428b88a602c7002883a2674ebe8248f7b5b302f
Palinody <lucas.klepic@protonmail.com>
2023-06-18 21:21:10 +0200

    feat: range_search_around_query_index for KDTreeIndexed

--- test/src/containers/kdtree/KDTreeIndexedTest.cpp
+++ test/src/containers/kdtree/KDTreeIndexedTest.cpp
@@ -689,6 +689,67 @@ TEST_F(KDTreeIndexedErrorsTest, KDBoundingBoxCountSampleTest) {
               << ", average neighbors number: " << total_neighbors / float(n_samples) << "\n";
 }
 
+TEST_F(KDTreeIndexedErrorsTest, KDBoundingBoxSearchSampleTest) {
+    common::timer::Timer<common::timer::Nanoseconds> timer;
+
+    fs::path filename = "varied.txt";
+
+    auto              data       = load_data<dType>(inputs_folder_ / filename, ' ');
+    const auto        labels     = load_data<std::size_t>(targets_folder_ / filename, ' ');
+    const std::size_t n_features = get_num_features_in_file(inputs_folder_ / filename);
+
+    const std::size_t n_samples = labels.size();
+
+    auto indices = generate_indices(n_samples);
+
+    std::cout << "n_elements: " << data.size() << "\n";
+    std::cout << "n_samples: " << n_samples << "\n";
+    std::cout << "n_features: " << n_features << "\n";
+
+    printf("Making the kdtree:\n");
+
+    timer.reset();
+
+    using IndicesIterator = decltype(indices)::iterator;
+    using SamplesIterator = decltype(data)::iterator;
+    // IndexedHighestVarianceBuild, IndexedMaximumSpreadBuild, IndexedCycleThroughAxesBuild
+    auto kdtree = ffcl::containers::KDTreeIndexed(
+        indices.begin(),
+        indices.end(),
+        data.begin(),
+        data.end(),
+        n_features,
+        ffcl::containers::KDTreeIndexed<IndicesIterator, SamplesIterator>::Options()
+            .bucket_size(std::sqrt(n_samples))
+            .max_depth(std::log2(n_samples))
+            .axis_selection_policy(kdtree::policy::IndexedMaximumSpreadBuild<IndicesIterator, SamplesIterator>())
+            .splitting_rule_policy(kdtree::policy::IndexedQuickselectMedianRange<IndicesIterator, SamplesIterator>()));
+
+    timer.print_elapsed_seconds(9);
+
+    std::vector<std::size_t> nn_indices;
+    std::vector<dType>       nn_distances;
+
+    const auto kd_bounding_box = BoundingBoxKDType<SamplesIterator>{{-0.1, 0.1}, {-0.1, 0.1}};
+
+    std::size_t total_neighbors = 0;
+
+    timer.reset();
+    for (std::size_t sample_index_query = 0; sample_index_query < n_samples; ++sample_index_query) {
+        std::tie(nn_indices, nn_distances) =
+            kdtree.range_search_around_query_index(indices[sample_index_query], kd_bounding_box);
+
+        total_neighbors += nn_indices.size();
+    }
+    timer.print_elapsed_seconds(9);
+
+    std::cout << "range_search_around_query_index (indices, distances)\n";
+    print_data(nn_indices, nn_indices.size());
+    print_data(nn_distances, nn_distances.size());
+    std::cout << "Total neighbors number: " << total_neighbors
+              << ", average neighbors number: " << total_neighbors / float(n_samples) << "\n";
+}
+
 TEST_F(KDTreeIndexedErrorsTest, MNISTTest) {
     fs::path filename = "mnist.txt";
 
