7a168c79678fe09fd6cd5fea33fc8642c3e33d16
Palinody <lucas.klepic@protonmail.com>
2023-09-14 00:24:41 +0200

    fix find in union find algo.

--- include/ffcl/containers/single_linkage_cluster_tree/SingleLinkageClusterTree.hpp
+++ include/ffcl/containers/single_linkage_cluster_tree/SingleLinkageClusterTree.hpp
@@ -4,6 +4,8 @@
 
 #include <numeric>
 
+#include <iostream>
+
 namespace ffcl {
 
 template <typename SampleIndexType, typename SampleValueType>
@@ -14,39 +16,56 @@ class SingleLinkageClusterTree {
     class UnionFind {
       public:
         UnionFind(std::size_t n_samples)
-          : parents_{std::make_unique<SampleIndexType[]>(n_samples)}
+          : n_samples_{n_samples}
+          , parents_{std::make_unique<SampleIndexType[]>(n_samples)}
           , ranks_{std::make_unique<SampleIndexType[]>(n_samples)} {
             std::iota(parents_.get(), parents_.get() + n_samples, static_cast<SampleIndexType>(0));
         }
 
-        SampleIndexType find(SampleIndexType index) const {
-            while (index != parents_[index]) {
-                index = parents_[index];
+        SampleIndexType find(const SampleIndexType& index) {
+            if (parents_[index] != index) {
+                // Recursively find the representative
+                parents_[index] = find(parents_[index]);
             }
             return index;
         }
 
         bool merge(const SampleIndexType& index_1, const SampleIndexType& index_2) {
-            const auto parent_1 = find(index_1);
-            const auto parent_2 = find(index_2);
+            const auto representative_1 = find(index_1);
+            const auto representative_2 = find(index_2);
 
-            if (parent_1 == parent_2) {
+            if (representative_1 == representative_2) {
                 return false;
             }
-            if (ranks_[parent_1] < ranks_[parent_2]) {
-                parents_[parent_1] = parent_2;
+            if (ranks_[representative_1] < ranks_[representative_2]) {
+                parents_[representative_1] = representative_2;
 
-            } else if (ranks_[parent_1] > ranks_[parent_2]) {
-                parents_[parent_2] = parent_1;
+            } else if (ranks_[representative_1] > ranks_[representative_2]) {
+                parents_[representative_2] = representative_1;
 
             } else {
-                parents_[parent_2] = parent_1;
-                ++ranks_[parent_1];
+                parents_[representative_2] = representative_1;
+                ++ranks_[representative_1];
             }
             return true;
         }
 
+        void print() const {
+            std::cout << "Parents:\n";
+            for (std::size_t index = 0; index < n_samples_; ++index) {
+                std::cout << parents_[index] << " ";
+            }
+            std::cout << "\n";
+
+            std::cout << "Ranks:\n";
+            for (std::size_t index = 0; index < n_samples_; ++index) {
+                std::cout << ranks_[index] << " ";
+            }
+            std::cout << "\n---\n";
+        }
+
       private:
+        std::size_t                        n_samples_;
         std::unique_ptr<SampleIndexType[]> parents_;
         std::unique_ptr<SampleIndexType[]> ranks_;
     };
@@ -59,6 +78,14 @@ class SingleLinkageClusterTree {
     SingleLinkageClusterTree(MinimumSpanningTreeType&& mst)
       : sorted_mst_{ffcl::mst::sort(std::move(mst))} {
         ffcl::mst::print(sorted_mst_);
+
+        UnionFind union_find(mst.size() + 1);
+
+        union_find.print();
+        for (const auto& edge : sorted_mst_) {
+            union_find.merge(std::get<0>(edge), std::get<1>(edge));
+            union_find.print();
+        }
     }
 
   private:
