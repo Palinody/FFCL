fcd5ecdfc01b09fd18023e7df66bac7779ae8fd0
Palinody <lucas.klepic@protonmail.com>
2023-09-30 15:42:04 +0200

    refacto

--- include/ffcl/containers/kdtree/policy/IndexedSplittingRulePolicy.hpp
+++ include/ffcl/containers/kdtree/policy/IndexedSplittingRulePolicy.hpp
@@ -4,55 +4,58 @@
 
 namespace kdtree::policy {
 
-template <typename RandomAccessIntIterator, typename RandomAccessIterator>
+template <typename IndicesIterator, typename SamplesIterator>
 struct IndexedSplittingRulePolicy {
     IndexedSplittingRulePolicy() = default;
 
     inline virtual std::tuple<std::size_t,
-                              ffcl::bbox::IteratorPairType<RandomAccessIntIterator>,
-                              ffcl::bbox::IteratorPairType<RandomAccessIntIterator>,
-                              ffcl::bbox::IteratorPairType<RandomAccessIntIterator>>
-    operator()(RandomAccessIntIterator index_first,
-               RandomAccessIntIterator index_last,
-               RandomAccessIterator    samples_first,
-               RandomAccessIterator    samples_last,
-               std::size_t             n_features,
-               std::size_t             feature_index) const = 0;
+                              ffcl::bbox::IteratorPairType<IndicesIterator>,
+                              ffcl::bbox::IteratorPairType<IndicesIterator>,
+                              ffcl::bbox::IteratorPairType<IndicesIterator>>
+    operator()(IndicesIterator index_first,
+               IndicesIterator index_last,
+               SamplesIterator samples_first,
+               SamplesIterator samples_last,
+               std::size_t     n_features,
+               std::size_t     feature_index) const = 0;
 };
 
-template <typename RandomAccessIntIterator, typename RandomAccessIterator>
-struct IndexedQuickselectMedianRange
-  : public IndexedSplittingRulePolicy<RandomAccessIntIterator, RandomAccessIterator> {
+template <typename IndicesIterator, typename SamplesIterator>
+struct IndexedQuickselectMedianRange : public IndexedSplittingRulePolicy<IndicesIterator, SamplesIterator> {
     inline std::tuple<std::size_t,
-                      ffcl::bbox::IteratorPairType<RandomAccessIntIterator>,
-                      ffcl::bbox::IteratorPairType<RandomAccessIntIterator>,
-                      ffcl::bbox::IteratorPairType<RandomAccessIntIterator>>
-    operator()(RandomAccessIntIterator index_first,
-               RandomAccessIntIterator index_last,
-               RandomAccessIterator    samples_first,
-               RandomAccessIterator    samples_last,
-               std::size_t             n_features,
-               std::size_t             feature_index) const;
+                      ffcl::bbox::IteratorPairType<IndicesIterator>,
+                      ffcl::bbox::IteratorPairType<IndicesIterator>,
+                      ffcl::bbox::IteratorPairType<IndicesIterator>>
+    operator()(IndicesIterator index_first,
+               IndicesIterator index_last,
+               SamplesIterator samples_first,
+               SamplesIterator samples_last,
+               std::size_t     n_features,
+               std::size_t     feature_index) const;
 };
 
 }  // namespace kdtree::policy
 
 namespace kdtree::policy {
 
-template <typename RandomAccessIntIterator, typename RandomAccessIterator>
+template <typename IndicesIterator, typename SamplesIterator>
 std::tuple<std::size_t,
-           ffcl::bbox::IteratorPairType<RandomAccessIntIterator>,
-           ffcl::bbox::IteratorPairType<RandomAccessIntIterator>,
-           ffcl::bbox::IteratorPairType<RandomAccessIntIterator>>
-IndexedQuickselectMedianRange<RandomAccessIntIterator, RandomAccessIterator>::operator()(
-    RandomAccessIntIterator index_first,
-    RandomAccessIntIterator index_last,
-    RandomAccessIterator    samples_first,
-    RandomAccessIterator    samples_last,
-    std::size_t             n_features,
-    std::size_t             feature_index) const {
+           ffcl::bbox::IteratorPairType<IndicesIterator>,
+           ffcl::bbox::IteratorPairType<IndicesIterator>,
+           ffcl::bbox::IteratorPairType<IndicesIterator>>
+IndexedQuickselectMedianRange<IndicesIterator, SamplesIterator>::operator()(IndicesIterator index_first,
+                                                                            IndicesIterator index_last,
+                                                                            SamplesIterator samples_first,
+                                                                            SamplesIterator samples_last,
+                                                                            std::size_t     n_features,
+                                                                            std::size_t     feature_index) const {
     return kdtree::algorithms::quickselect_median_indexed_range(
-        index_first, index_last, samples_first, samples_last, n_features, feature_index);
+        /**/ index_first,
+        /**/ index_last,
+        /**/ samples_first,
+        /**/ samples_last,
+        /**/ n_features,
+        /**/ feature_index);
 }
 
 }  // namespace kdtree::policy
