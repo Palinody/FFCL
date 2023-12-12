a6b9a3e2631934101ee06a9912fb85a7f6570b01
Palinody <lucas.klepic@protonmail.com>
2023-12-12 17:52:22 +0100

    feat: improved timer

--- include/ffcl/common/Timer.hpp
+++ include/ffcl/common/Timer.hpp
@@ -22,19 +22,21 @@ class Timer {
   public:
     Timer();
 
-    inline std::uint64_t get_now() const;
+    inline auto get_now() const;
 
     inline void reset();
 
-    inline std::uint64_t elapsed() const;
+    inline auto elapsed() const;
 
     template <typename SleepDurationType>
-    inline void sleep(std::uint64_t duration) const;
+    inline void sleep(std::chrono::seconds::rep duration) const;
 
-    inline void print_elapsed_seconds(std::uint8_t n_decimals = 3) const;
+    inline void print_elapsed_seconds(std::uint8_t n_decimals = get_num_decimals()) const;
 
   private:
-    std::uint64_t timestamp_;
+    static constexpr std::uint8_t get_num_decimals();
+
+    std::chrono::seconds::rep timestamp_;
 };
 
 template <typename DurationType>
@@ -42,7 +44,7 @@ Timer<DurationType>::Timer()
   : timestamp_{get_now()} {}
 
 template <typename DurationType>
-std::uint64_t Timer<DurationType>::get_now() const {
+auto Timer<DurationType>::get_now() const {
     return std::chrono::duration_cast<DurationType>(ClockType::now().time_since_epoch()).count();
 }
 
@@ -52,19 +54,19 @@ void Timer<DurationType>::reset() {
 }
 
 template <typename DurationType>
-std::uint64_t Timer<DurationType>::elapsed() const {
+auto Timer<DurationType>::elapsed() const {
     return get_now() - timestamp_;
 }
 
 template <typename DurationType>
 template <typename SleepDurationType>
-void Timer<DurationType>::sleep(std::uint64_t duration) const {
+void Timer<DurationType>::sleep(std::chrono::seconds::rep duration) const {
     std::this_thread::sleep_for(static_cast<SleepDurationType>(duration));
 }
 
 template <typename DurationType>
 void Timer<DurationType>::print_elapsed_seconds(std::uint8_t n_decimals) const {
-    const std::uint64_t elapsed = this->elapsed();
+    const auto elapsed = this->elapsed();
 
     if constexpr (std::is_same_v<DurationType, Seconds>) {
         printf("Elapsed (s): %.*f\n", n_decimals, static_cast<float>(elapsed));
@@ -84,4 +86,35 @@ void Timer<DurationType>::print_elapsed_seconds(std::uint8_t n_decimals) const {
     }
 }
 
+template <typename DurationType>
+constexpr std::uint8_t Timer<DurationType>::get_num_decimals() {
+    if constexpr (std::is_same_v<DurationType, std::chrono::seconds>) {
+        return 0;
+
+    } else if constexpr (std::is_same_v<DurationType, std::chrono::milliseconds>) {
+        return 3;
+
+    } else if constexpr (std::is_same_v<DurationType, std::chrono::microseconds>) {
+        return 6;
+
+    } else if constexpr (std::is_same_v<DurationType, std::chrono::nanoseconds>) {
+        return 9;
+
+    } else {
+        // Default number of decimals for unsupported types
+        return 3;
+    }
+}
+
 }  // namespace ffcl::common
+
+/*
+void example() {
+    // set a timer object with a millisecond time precision
+    ffcl::common::Timer<ffcl::common::Milliseconds> timer;
+    // set the amount of time to wait w.r.t. the time unit. Accepts only integer so choose the right unit depending on
+    // the precision needed: Seconds, Milliseconds, Microseconds, Nanoseconds
+    timer.sleep<ffcl::common::Milliseconds>(4500);
+    timer.print_elapsed_seconds(9);
+}
+*/
